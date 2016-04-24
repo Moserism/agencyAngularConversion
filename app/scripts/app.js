bba00519f74c5d972686e17aa2dde74b599c1a6e
Michael Opper Moser <michael@moserism.com>
2016-04-24 10:11:12 -0400

    angularized services, angularization step 1 portfolios

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,76 @@
  *
  * Main module of the application.
  */
-angular
-  .module('agencyAngularConversionApp', []);
+
+var app = angular.module('agencyAngularConversionApp', [ ]);
+
+var serviceList = [{
+	name: 'E-Commerce',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.',
+	icon: 'fa-shopping-cart'
+	},
+{
+	name: 'Responsive Design',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.',
+	icon: 'fa-laptop'
+	},
+{
+	name: 'Web Security',
+	description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.',
+	icon: 'fa-lock'
+	}
+];
+
+var productImageDirectory = '../images/portfolio/';
+
+var portfolioList = [
+	{
+		url: 		'../views/modals/modal_1.html',
+		name: 		'Round Icons',
+		category: 	'Graphic Design',
+		image:		productImageDirectory + 'roundicons.png'
+	},
+	
+	{
+		url: 		'../views/modals/modal_2.html',
+		name: 		'',
+		category: 	'',
+		image:		''
+	},
+	
+	{
+		url: 		'../views/modals/modal_3.html',
+		name: 		'',
+		category: 	'',
+		image:		''
+	},
+	
+	{
+		url: 		'../views/modals/modal_4.html',
+		name: 		'',
+		category: 	'',
+		image:		''
+	},
+	
+	{
+		url: 		'../views/modals/modal_5.html',
+		name: 		'',
+		category: 	'',
+		image:		''
+	},
+	
+	{
+		url: 		'../views/modals/modal_6.html',
+		name: 		'',
+		category: 	'',
+		image:		''
+	}
+];
+
+app.controller('ServicesController', function () {
+	this.services = serviceList;
+});
+
+app.controller('PortfolioController', function(){
+	this.portfolio = portfolioList;
+});
